abf0cf3330c1de4929dd8bf44a2eec8b9f6654fe
nagendrasomala <105587751+nagendrasomala@users.noreply.github.com>
2023-09-09 20:28:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ const getUserCoordinates = () => {
     navigator.geolocation.getCurrentPosition(
         position =>{
             const {latitude,longitude} = position.coords;
-            const REVERSE_GEOCODING_URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
+            const REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
             fetch(REVERSE_GEOCODING_URL).then(res => res.json()).then(data =>{
                 if(!data.length) return alert("an alert occured");
                 const {name} = data[0];
